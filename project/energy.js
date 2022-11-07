997ef2d50a3968f3ade09f755f819da1cace7a1b
Jheim12 <jonjonas.heim@gmail.com>
2022-11-07 14:51:31 -0600

    barchart works!

--- project/energy.js
+++ project/energy.js
@@ -2,7 +2,8 @@ d3.csv("trains_switzerland.csv").then(data => {
 
     const height = 500,
         width = 800,
-        margin = ({ top: 15, right: 30, bottom: 35, left: 40 })
+        horizon = height/2,
+        margin = ({ top: 15, right: 30, bottom: 35, left: 50 }),
         innerWidth = width - margin.left - margin.right;
     
     const svg = d3.select("#energy")
@@ -24,23 +25,36 @@ d3.csv("trains_switzerland.csv").then(data => {
                 .range([margin.left, width - margin.right])
         .padding(0.1);
     
-    const y = d3.scaleLinear()
+    const y_pkm = d3.scaleLinear()
         .domain([0, d3.max(data, d => d.passenger_km)]).nice()
-        .range([height - margin.bottom, margin.top]);
+        .range([horizon, 0]);
+
+    const y_energy = d3
+        .scaleLinear()
+        .domain([0, d3.max(data, (d) => d.total_passenger_energy_consumption)])
+        .range([horizon, height - margin.bottom]);
     
     const xAxis = g => g
         .attr("transform", `translate(0,${height - margin.bottom + 5})`)
         .call(d3.axisBottom(x));
     
-    const yAxis = g => g
+    const yAxis_pkm = g => g
         .attr("transform", `translate(${margin.left - 5},0)`)
-        .call(d3.axisLeft(y))
+        .call(d3.axisLeft(y_pkm))
+        
+    const yAxis_enery = g => g
+        .attr("transform", `translate(${margin.left - 5},0)`)
+        .call(d3.axisLeft(y_energy))
+        
 
     svg.append("g")
         .call(xAxis);
     
     svg.append("g")
-        .call(yAxis);
+        .call(yAxis_pkm);
+    
+    svg.append("g")
+        .call(yAxis_enery);
 
     let bar = svg.selectAll(".bar")
         .append("g")
@@ -52,13 +66,27 @@ d3.csv("trains_switzerland.csv").then(data => {
         .attr("fill", "steelblue")
         .attr("x", d => x(d.year))
         .attr("width", x.bandwidth())
-        .attr("y", d => y(d.passenger_km))
-        .attr("height", d => y(0) - y(d.passenger_km))
+        .attr("y", d => y_pkm(d.passenger_km))
+        .attr("height", d => y_pkm(0) - y_pkm(d.passenger_km))
     
     bar.append("text")
         .text(d => d.passenger_km)
         .attr('x', d => x(d.year) + (x.bandwidth()/2))
-        .attr('y', d => y(d.passenger_km) + 15)
+        .attr('y', d => y_pkm(d.passenger_km) + 15)
+        .attr('text-anchor', 'middle')
+        .style('fill', 'white');   
+    
+    bar.append("rect")
+        .attr("fill", "blue")
+        .attr("x", d => x(d.year))
+        .attr("width", x.bandwidth())
+        .attr("y", d => y_energy(0))
+        .attr("height", d => y_energy(d.total_passenger_energy_consumption) - y_energy(0))
+    
+    bar.append("text")
+        .text(d => d.passenger_km)
+        .attr('x', d => x(d.year) + (x.bandwidth()/2))
+        .attr('y', d => y_energy(d.total_passenger_energy_consumption) - 15)
         .attr('text-anchor', 'middle')
         .style('fill', 'white');   
 });
