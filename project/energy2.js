3102c1e6ab83cd65ac5fbd67a426d9ea3eda6282
Jheim12 <jonjonas.heim@gmail.com>
2022-11-27 13:08:26 -0600

    prototype

--- project/energy2.js
+++ project/energy2.js
@@ -14,6 +14,7 @@ d3.csv("trains_switzerland.csv").then(data => {
     for (let d of data) {
         d.year = +d.year
         d.passenger_km = +d.passenger_km            // Change to train_km?!
+        d.train_km = +d.train_km
         d.total_passenger_energy_consumption = +d.total_passenger_energy_consumption
         d.energy_consumption_per_passenger = +d.energy_consumption_per_passenger;
     }
@@ -26,6 +27,10 @@ d3.csv("trains_switzerland.csv").then(data => {
     
     const y_pkm = d3.scaleLinear()
         .domain([0, d3.max(data, d => d.passenger_km)]).nice()
+        .range([horizon - 5, margin.top]);
+
+    const y_tkm = d3.scaleLinear()
+        .domain([0, d3.max(data, d => d.train_km)]).nice()
         .range([horizon - 5, margin.top]);
 
     const y_energy = d3.scaleLinear()
@@ -47,7 +52,12 @@ d3.csv("trains_switzerland.csv").then(data => {
         .attr("transform", `translate(${margin.left}, 0)`)
         .call(d3.axisLeft(y_pkm))
         .call(d3.axisLeft(y_pkm).tickSizeOuter(0));
-        
+     
+    const yAxis_tkm = g => g
+        .attr("transform", `translate(${width-margin.right},0)`)
+        .call(d3.axisRight(y_tkm))
+        .call(d3.axisRight(y_tkm).tickSizeOuter(0));
+
     const yAxis_energy = g => g
         .attr("transform", `translate(${margin.left}, 0)`)
         .call(d3.axisLeft(y_energy))
@@ -65,6 +75,11 @@ d3.csv("trains_switzerland.csv").then(data => {
     
     svg.append("g")
         .call(yAxis_pkm);
+    
+    const top_right_axis = svg.append("g")
+        .attr('class', 'axisRed')
+        .call(yAxis_tkm)   
+        .style("opacity", 0);
     
     svg.append("g")
         .call(yAxis_energy);
@@ -93,6 +108,17 @@ d3.csv("trains_switzerland.csv").then(data => {
         .text("Million Pkm")
         .style("font-size","15px");
     
+    const yRightTopLabel = svg.append("text")
+        .attr("class", "y-label")
+        .attr("text-anchor", "middle")
+        .attr("x", (horizon + margin.top)/2)
+        .attr("y", -width + margin.right - 30)
+        .attr("transform", "rotate(90)")
+        .text("Million Tkm")
+        .style("font-size","15px")
+        .style("fill", "red")
+        .style("opacity", 0);
+    
     svg.append("text")
         .attr("class", "y-label")
         .attr("text-anchor", "middle")
@@ -102,7 +128,7 @@ d3.csv("trains_switzerland.csv").then(data => {
         .text("Total GWh")
         .style("font-size","15px");
     
-    const yRightLabel = svg.append("text")
+    const yRightBottomLabel = svg.append("text")
         .attr("class", "y-label")
         .attr("text-anchor", "middle")
         .attr("x", (horizon + height - margin.bottom)/2)
@@ -149,16 +175,29 @@ d3.csv("trains_switzerland.csv").then(data => {
         .attr('text-anchor', 'middle')
         .style('fill', 'white');   
     
-    // Build Line
-    let line = d3.line()
+    // Build Lines
+    let tkm_line = d3.line()
+        .x(d => x(d.year) + (x.bandwidth()/2))
+        .y(d => y_tkm(d.train_km));
+
+    let energy_pc_line = d3.line()
         .x(d => x(d.year) + (x.bandwidth()/2))
         .y(d => y_energy_pc(d.energy_consumption_per_passenger));
 
-    // Add Line
+    // Add Lines
     bar.append("path")
         .attr("fill", "none")
+        .attr("id", "tkm")
         .attr("stroke", "red")
-        .attr("d", line(data))
+        .attr("d", tkm_line(data))
+        .attr("stroke-width", 1)
+        .style("opacity", 0);
+
+    bar.append("path")
+        .attr("id", "energy_pc")
+        .attr("fill", "none")
+        .attr("stroke", "red")
+        .attr("d", energy_pc_line(data))
         .attr("stroke-width", 1)
         .style("opacity", 0);
 
@@ -169,16 +208,33 @@ d3.csv("trains_switzerland.csv").then(data => {
     function update() {
         const switchChecked = d3.select("#energy_checkbox").property("checked");
           
-        const lineData = switchChecked
+        const lineData_energy_pc = switchChecked
             ? data
             : Array.from(data, (d) => {
                 return { ...d, year: d.year, energy_consumption_per_passenger: 0};
                 });
+        
+        const lineData_tkm = switchChecked
+            ? data
+            : Array.from(data, (d) => {
+                return { ...d, year: d.year, train_km: 0};
+                });
     
         bar.transition()
             .duration(1000)
-            .selectAll("path")
-            .attr("d", line(lineData))
+            .select("#tkm")
+            .attr("d", tkm_line(lineData_tkm))
+            .style("opacity", switchChecked ? 1 : 0);
+
+        bar.transition()
+            .duration(1000)
+            .select("#energy_pc")
+            .attr("d", energy_pc_line(lineData_energy_pc))
+            .style("opacity", switchChecked ? 1 : 0);
+
+        top_right_axis
+            .transition()
+            .duration(1000)
             .style("opacity", switchChecked ? 1 : 0);
 
         bottom_right_axis
@@ -186,7 +242,12 @@ d3.csv("trains_switzerland.csv").then(data => {
             .duration(1000)
             .style("opacity", switchChecked ? 1 : 0);
         
-        yRightLabel
+        yRightTopLabel
+            .transition()
+            .duration(1000)
+            .style("opacity", switchChecked ? 1 : 0);
+        
+        yRightBottomLabel
             .transition()
             .duration(1000)
             .style("opacity", switchChecked ? 1 : 0);
@@ -198,6 +259,6 @@ d3.csv("trains_switzerland.csv").then(data => {
         .attr("text-anchor", "middle")
         .attr("x", width/2)
         .attr("y", margin.top - 20)
-        .text("Unproportional Energy Consumption")
+        .text("Unproportional Energy Consumption 2")
         .style("font-size","26px");
 });
